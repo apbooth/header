1e6fbac30bf6d11c441abe6321c77bbdb40b8ed3
apbooth <166928947+apbooth@users.noreply.github.com>
2024-12-11 16:09:41 +0000

    compiling with header files

---
+++ header/calculate.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int calculate(int number) {
+    return number * 2;
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
+#include "header/calculate.h"
 
+using namespace std;
+
+int main() {
+    cout << calculate(60) << endl;
+    return 0;
+}
